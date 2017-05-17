668068147c19a024cf543f94b36de45759eeee96
Jaime González García <jaime.gg83@gmail.com>
2017-05-17 08:21:25 +0200

    Added form in person details and two way data binding

--- src/app/person-details/person-details.component.ts
+++ src/app/person-details/person-details.component.ts
@@ -6,17 +6,7 @@ import { Person } from "../person";
 
 @Component({
   selector: 'app-person-details',
-  template: `
-  <section *ngIf="person">
-    <h2>You selected:  {{person.name}}</h2>
-    <h3>Description</h3>
-    <p>
-       {{person.name}} weights {{person.weight}} and is {{person.height}} tall.
-    </p>
-  </section>
-
-  <button (click)="gotoPeoplesList()">Back to peoples list</button>
-  `,
+  templateUrl: './person-details.component.html',
   styles: []
 })
 export class PersonDetailsComponent implements OnInit, OnDestroy {
